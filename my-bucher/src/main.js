e7c6e2de2a6ba83e4662ed2cdd54dde8535d24df
Andre Felizardo <andreluis.felizardo@outlook.com>
2018-01-11 16:38:39 -0200

    Create a initial view with the book list example

--- my-bucher/src/main.js
+++ my-bucher/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
--- my-bucher/src/router/index.js
+++ my-bucher/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ListBooks from '@/components/ListBooks'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'ListBooks',
+      component: ListBooks
     }
   ]
 })
