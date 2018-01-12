fb1c0e5d07c1dacd307e32f09bcd99b11f3d8c61
Andre Felizardo <andreluis.felizardo@outlook.com>
2018-01-12 00:02:23 -0200

    Mock books

--- my-bucher/src/components/ListBooks.vue
+++ my-bucher/src/components/ListBooks.vue
@@ -12,73 +12,23 @@
             </div>
             <div class="md-row">
                 <div class="md-layout md-gutter md-alignment-top-center">
-                    <div class="md-layout-item md-size-25">
+                    <div class="md-layout-item md-size-25" v-for="book in books">
                         <md-card>
                             <md-card-media-cover md-solid>
-                                <md-card-media md-ratio="1:2">
-                                    <img src="../assets/lei-do-triunfo.jpg" alt="Skyscraper">
+                                <md-card-media>
+                                    <img :src="`/static/images/${book.cover}`" :alt="book.title">
                                 </md-card-media>
 
                                 <md-card-area>
                                 <md-card-header>
-                                    <span class="md-title">A Lei do Triunfo</span>
-                                    <span class="md-subtitle">Napoleon Hill</span>
+                                    <span class="md-title">{{ book.title }}</span>
+                                    <span class="md-subtitle">{{ book.author }}</span>
                                 </md-card-header>
 
-                                <md-card-content>
-                                    Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.
-                                </md-card-content>
+                                <md-card-content>{{ book.description }}</md-card-content>
 
                                 <md-card-actions>
-                                    <md-button>Emprestar</md-button>
-                                </md-card-actions>
-                                </md-card-area>
-                            </md-card-media-cover>
-                        </md-card>
-                    </div>
-                    <div class="md-layout-item md-size-25">
-                        <md-card>
-                            <md-card-media-cover md-solid>
-                                <md-card-media md-ratio="1:2">
-                                    <img src="../assets/lei-do-triunfo.jpg" alt="A Lei do Triunfo">
-                                </md-card-media>
-
-                                <md-card-area>
-                                <md-card-header>
-                                    <span class="md-title">A Lei do Triunfo</span>
-                                    <span class="md-subtitle">Napoleon Hill</span>
-                                </md-card-header>
-
-                                <md-card-content>
-                                    Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.
-                                </md-card-content>
-
-                                <md-card-actions>
-                                    <md-button>Emprestar</md-button>
-                                </md-card-actions>
-                                </md-card-area>
-                            </md-card-media-cover>
-                        </md-card>
-                    </div>
-                    <div class="md-layout-item md-size-25">
-                        <md-card>
-                            <md-card-media-cover md-solid>
-                                <md-card-media md-ratio="1:2">
-                                    <img src="../assets/lei-do-triunfo.jpg" alt="Skyscraper">
-                                </md-card-media>
-
-                                <md-card-area>
-                                <md-card-header>
-                                    <span class="md-title">A Lei do Triunfo</span>
-                                    <span class="md-subtitle">Napoleon Hill</span>
-                                </md-card-header>
-
-                                <md-card-content>
-                                    Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.
-                                </md-card-content>
-
-                                <md-card-actions>
-                                    <md-button>Emprestar</md-button>
+                                    <md-button v-if="!book.loan">Emprestar</md-button>
                                 </md-card-actions>
                                 </md-card-area>
                             </md-card-media-cover>
@@ -92,13 +42,42 @@
 
 <script>
 export default {
-  name: 'ListBooks'
+  name: 'ListBooks',
+
+    data () {
+        return {
+            books: [
+                {
+                    title: 'A lei do triunfo',
+                    author: 'Napoleon Hill',
+                    description: 'Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.',
+                    cover: 'lei-do-triunfo.jpg',
+                    loan: false,
+                    read: false 
+                },
+                {
+                    title: 'A Startup enxuta',
+                    author: 'Eric Ries',
+                    description: 'Como os empreendedores atuais utilizam a inovação contínua para criar empresas extremamente bem-sucedidas.',
+                    cover: 'startup-enxuta.jpg',
+                    loan: false,
+                    read: false 
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+    .md-card-media img {
+        min-height: 400px
+    }
+    .md-card-content {
+        max-height: 80px;
+    }
 </style>
+
 
 <style>
 .menu-icon {
