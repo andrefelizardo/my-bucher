ddb8f2afa12d048ab75e2aa3aa6b3d66a4b4f940
andre.luis.felizardo <andre.luis.felizardo@oi.com.br>
2018-01-20 10:54:51 -0200

    Remove bootstrap and vee-validate

--- src/main.js
+++ src/main.js
@@ -4,12 +4,10 @@ import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-// import VeeValidate from 'vee-validate'
 
 Vue.config.productionTip = false
 
 Vue.use(VueMaterial)
-// Vue.use(VeeValidate)
 
 /* eslint-disable no-new */
 new Vue({
