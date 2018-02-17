5e686b048c2e35f18801ba9c34a8698fe576f8c1
André Felizardo <andreluis.felizardo@outlook.com>
2018-02-17 15:58:59 -0200

    WIP: API working

---
+++ API/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+const indexRoute = require('./routes/index-route')
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  next()
+})
+
+app.use('/', indexRoute)
+
+module.exports = app
